cb557e106eee7065c375a59142f0d4e97324b070
jsun969 <i@jsun969.cn>
2021-06-05 13:41:55 +0800

    :sparkles: Add alarm and location in submit

--- submit.php
+++ submit.php
@@ -1,3 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>留言成功</title>
+</head>
+<body>
 <?php
 include './config.php';
 $conn = new mysqli(db_host, db_username, db_password, db_name, db_port);
@@ -10,4 +20,10 @@ if ($stmt = $conn->prepare("INSERT INTO message (`is_anonymous`,`ip`,`name`,`ema
   $message = $_POST["message"];
   $stmt->execute();
 }
-echo '<h1>留言成功</h1>';
+?>
+<script type="text/javascript">
+  alert('留言成功');
+  window.location = "index.php";
+</script>
+</body>
+</html>
