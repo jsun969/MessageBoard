573833d441ce17914fc438f37ce4a42028ec40b1
jsun969 <i@jsun969.cn>
2021-06-05 11:59:21 +0800

    :poop: Use php file to prevent repeatedly submit

--- submit.php
+++ submit.php
@@ -1,1 +1,13 @@
 <?php
+include './config.php';
+$conn = new mysqli(db_host, db_username, db_password, db_name, db_port);
+if ($stmt = $conn->prepare("INSERT INTO message (`is_anonymous`,`ip`,`name`,`email`,`message`) VALUES (?,?,?,?,?)")) {
+  $stmt->bind_param("issss", $isAnonymous, $ip, $name, $email, $message);
+  $isAnonymous = (bool)$_POST["isAnonymous"];
+  $ip = $_SERVER['REMOTE_ADDR'];
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+  $stmt->execute();
+}
+echo '<h1>留言成功</h1>';
