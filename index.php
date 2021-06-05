af527957831bf9ece16caa1bda0eca3576b02890
jsun969 <i@jsun969.cn>
2021-06-05 10:39:15 +0800

    :sparkles: Save form data to database

--- index.php
+++ index.php
@@ -21,6 +21,19 @@
   </div>
 </header>
 <main class="mdui-container">
+  <?php
+  include './config.php';
+  $conn = new mysqli(db_host, db_username, db_password, db_name, db_port);
+  if ($stmt = $conn->prepare("INSERT INTO message (`is_anonymous`,`ip`,`name`,`email`,`message`) VALUES (?,?,?,?,?)")) {
+    $stmt->bind_param("issss", $isAnonymous, $ip, $name, $email, $message);
+    $isAnonymous = (bool)$_POST["isAnonymous"];
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    $stmt->execute();
+  }
+  ?>
   <form action="" method="post" class="mdui-m-b-2 mdui-col-xs-12">
     <div class="mdui-textfield mdui-textfield-floating-label mdui-col-xs-6 userInfo">
       <i class="mdui-icon material-icons">account_circle</i>
